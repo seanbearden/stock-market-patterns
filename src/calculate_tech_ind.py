bb046772e4eda1733fa5d421419042be6a31e1dc
seanbearden <72461227+seanbearden@users.noreply.github.com>
2024-01-12 13:27:42 -0800

    Update to progress

--- src/calculate_tech_ind.py
+++ src/calculate_tech_ind.py
@@ -2,14 +2,15 @@ import os
 import pandas as pd
 import psycopg2
 import talib
-from tools.database_helper import get_all_ticker_symbols
+from tools.database_helper import create_stock_database_tables
 from tools.pattern_helper import calculate_ichimoku, calculate_rmi
 from io import StringIO
 
 
 # Define the table name
 stock_quotes_daily_table = 'stock_quotes_daily'
-stock_quotes_daily_ti_table = 'stock_quotes_daily_ti'
+stock_quotes_daily_adj_table = 'stock_quotes_daily_adj'
+stock_quotes_daily_ti_table = f'{stock_quotes_daily_adj_table}_ti'
 sma_periods = [20, 50, 200]
 update = False
 
@@ -28,125 +29,139 @@ conn = psycopg2.connect(**db_params)
 # Open a cursor to perform database operations
 cur = conn.cursor()
 
-drop_columns = ['high', 'low', 'close', 'volume']
+# Using the with statement for managing the connection
+with psycopg2.connect(**db_params) as conn:
+    with conn.cursor() as cur:
 
-# Fetch distinct ticker ids
-cur.execute("""SELECT DISTINCT ticker_id FROM tickers""")
-ticker_ids = cur.fetchall()
+        tables = create_stock_database_tables(conn, cur)
 
-for ticker_id in ticker_ids:
+        drop_columns = ['date', 'open', 'high', 'low', 'close', 'volume']
 
-    # Apply split coefficient to past, but not to day of split
+        # Fetch distinct ticker ids
+        cur.execute("""SELECT DISTINCT ticker_id FROM tickers""")
+        ticker_ids = cur.fetchall()
 
-    query = f"""
-        WITH stock_date_adjusted AS (
-            SELECT
-                *, 
-                COALESCE(
-                    EXP(
-                        SUM(LN(split_coefficient)) OVER (
-                            ORDER BY date DESC ROWS BETWEEN UNBOUNDED PRECEDING AND 1 PRECEDING
-                        )
-                    ), 1
-                ) AS cumulative_split
-            FROM
-                stock_quotes_daily
-            WHERE 
-                ticker_id = {ticker_id[0]}
-        )
-        SELECT    
-            id,        
-            high / cumulative_split AS high,
-            low / cumulative_split AS low,
-            close / cumulative_split AS close,
-            volume * cumulative_split AS volume
-        FROM
-            stock_date_adjusted
-        WHERE 
-            ticker_id = {ticker_id[0]}
-        ORDER BY
-            date;
-    """
+        for ticker_id in ticker_ids:
 
-    cur.execute(query)
-    rows = cur.fetchall()
-    colnames = [desc[0] for desc in cur.description]
+            # Apply split coefficient to past, but not to day of split
+            query = f"""
+                WITH stock_date_adjusted AS (
+                    SELECT
+                        *, 
+                        COALESCE(
+                            EXP(
+                                SUM(LN(split_coefficient)) OVER (
+                                    ORDER BY date DESC ROWS BETWEEN UNBOUNDED PRECEDING AND 1 PRECEDING
+                                )
+                            ), 1
+                        ) AS cumulative_split
+                    FROM
+                        {stock_quotes_daily_table}
+                    WHERE 
+                        ticker_id = {ticker_id[0]}
+                )
+                SELECT    
+                    id,
+                    date,
+                    open / cumulative_split AS open,        
+                    high / cumulative_split AS high,
+                    low / cumulative_split AS low,
+                    close / cumulative_split AS close,
+                    volume * cumulative_split AS volume
+                FROM
+                    stock_date_adjusted
+                WHERE 
+                    ticker_id = {ticker_id[0]}
+                ORDER BY
+                    date;
+            """
 
-    df = pd.DataFrame(rows, columns=colnames)
+            cur.execute(query)
+            rows = cur.fetchall()
+            colnames = [desc[0] for desc in cur.description]
 
-    for timeperiod in sma_periods:
-        df[f'sma_{timeperiod}'] = talib.SMA(df['close'], timeperiod=timeperiod)
+            df = pd.DataFrame(rows, columns=colnames)
 
-    # Technical Indicators
-    timeperiod = 14
-    df[f'rsi_{timeperiod}'] = talib.RSI(df['close'], timeperiod=timeperiod)
-    df[f'mfi_{timeperiod}'] = talib.MFI(high=df['high'], low=df['low'], close=df['close'], volume=df['volume'],
-                                        timeperiod=timeperiod)
-    momentum_period = 5
-    df[f'rmi_{timeperiod}_{momentum_period}'] = calculate_rmi(
-        df['close'],
-        time_period=timeperiod,
-        momentum_period=momentum_period)
+            # Prepare the data to be inserted into PostgreSQL table
+            columns = df.columns.tolist()
+            output = StringIO()
+            df.to_csv(output, sep='\t', header=False, index=False)
+            output.seek(0)
 
-    fastperiod = 12
-    slowperiod = 26
-    signalperiod = 9
+            # Insert the DataFrame into the PostgreSQL table
+            cur.copy_from(output, stock_quotes_daily_adj_table, null='', columns=columns)
+            conn.commit()
 
-    macd = talib.MACD(df['close'], fastperiod=fastperiod, slowperiod=slowperiod, signalperiod=signalperiod)
-    df[f'macd_{fastperiod}_{slowperiod}_{signalperiod}'] = macd[0]
-    df[f'macd_signal_{fastperiod}_{slowperiod}_{signalperiod}'] = macd[1]
-    df[f'macd_hist_{fastperiod}_{slowperiod}_{signalperiod}'] = macd[2]
+            for timeperiod in sma_periods:
+                df[f'sma_{timeperiod}'] = talib.SMA(df['close'], timeperiod=timeperiod)
 
-    df = calculate_ichimoku(df, future=False)
+            # Technical Indicators
+            timeperiod = 14
+            df[f'rsi_{timeperiod}'] = talib.RSI(df['close'], timeperiod=timeperiod)
+            df[f'mfi_{timeperiod}'] = talib.MFI(high=df['high'], low=df['low'], close=df['close'], volume=df['volume'],
+                                                timeperiod=timeperiod)
+            momentum_period = 5
+            df[f'rmi_{timeperiod}_{momentum_period}'] = calculate_rmi(
+                df['close'],
+                time_period=timeperiod,
+                momentum_period=momentum_period)
 
-    df.rename(columns={
-        'tenkan_sen': 'ic_conversion_9_26_52',
-        'kijun_sen': 'ic_base_9_26_52',
-        'senkou_span_a': 'ic_span_a_9_26_52',
-        'senkou_span_b': 'ic_span_b_9_26_52'
-    }, inplace=True)
+            fastperiod = 12
+            slowperiod = 26
+            signalperiod = 9
 
-    df.drop(drop_columns + ['chikou_span'], axis=1, inplace=True)
+            macd = talib.MACD(df['close'], fastperiod=fastperiod, slowperiod=slowperiod, signalperiod=signalperiod)
+            df[f'macd_{fastperiod}_{slowperiod}_{signalperiod}'] = macd[0]
+            df[f'macd_signal_{fastperiod}_{slowperiod}_{signalperiod}'] = macd[1]
+            df[f'macd_hist_{fastperiod}_{slowperiod}_{signalperiod}'] = macd[2]
 
-    # Insert the DataFrame into the PostgreSQL table
+            df = calculate_ichimoku(df, future=False)
 
-    # which rows exist and need to be updated?
-    # get ids for ticker_id that exist in ti table
-    cur.execute(f"""
-        SELECT ti.id 
-        FROM {stock_quotes_daily_ti_table} as ti
-        INNER JOIN stock_quotes_daily as s
-        ON ti.id = s.id
-        WHERE s.ticker_id = {ticker_id[0]}
-        """)
-    existing_ids = [i[0] for i in cur.fetchall()]
+            df.rename(columns={
+                'tenkan_sen': 'ic_conversion_9_26_52',
+                'kijun_sen': 'ic_base_9_26_52',
+                'senkou_span_a': 'ic_span_a_9_26_52',
+                'senkou_span_b': 'ic_span_b_9_26_52'
+            }, inplace=True)
 
-    idx = df.id.isin(existing_ids)
-    df_existing = df[idx]
-    df_new = df[~idx]
+            df.drop(drop_columns + ['chikou_span'], axis=1, inplace=True)
 
-    # Define the PostgreSQL columns for the table
-    columns = df.columns.tolist()
+            # Insert the DataFrame into the PostgreSQL table
 
-    # Bulk insert new records
-    if not df_new.empty:
-        # Prepare the data to be inserted into PostgreSQL table
-        output = StringIO()
-        df.to_csv(output, sep='\t', header=False, index=False)
-        output.seek(0)
+            # which rows exist and need to be updated?
+            # get ids for ticker_id that exist in ti table
+            cur.execute(f"""
+                SELECT ti.id 
+                FROM {stock_quotes_daily_ti_table} as ti
+                INNER JOIN {stock_quotes_daily_table} as s
+                ON ti.id = s.id
+                WHERE s.ticker_id = {ticker_id[0]}
+                """)
+            existing_ids = [i[0] for i in cur.fetchall()]
 
-        # Insert the DataFrame into the PostgreSQL table
-        cur.copy_from(output, stock_quotes_daily_ti_table, null='', columns=columns)
-        conn.commit()
+            idx = df.id.isin(existing_ids)
+            df_existing = df[idx]
+            df_new = df[~idx]
 
-    # Update existing records
-    if update and not df_existing.empty:
-        for index, row in df_existing.iterrows():
-            update_query = (f"UPDATE {stock_quotes_daily_ti_table} SET " +
-                            ", ".join([f"{col} = %s" for col in columns if col != 'id']) +
-                            " WHERE id = %s")
-            cur.execute(update_query, tuple(row[col] for col in columns if col != 'id') + (row['id'],))
-        conn.commit()
+            # Define the PostgreSQL columns for the table
+            columns = df.columns.tolist()
 
-cur.close()
-conn.close()
+            # Bulk insert new records
+            if not df_new.empty:
+                # Prepare the data to be inserted into PostgreSQL table
+                output = StringIO()
+                df.to_csv(output, sep='\t', header=False, index=False)
+                output.seek(0)
+
+                # Insert the DataFrame into the PostgreSQL table
+                cur.copy_from(output, stock_quotes_daily_ti_table, null='', columns=columns)
+                conn.commit()
+
+            # Update existing records
+            if update and not df_existing.empty:
+                for index, row in df_existing.iterrows():
+                    update_query = (f"UPDATE {stock_quotes_daily_ti_table} SET " +
+                                    ", ".join([f"{col} = %s" for col in columns if col != 'id']) +
+                                    " WHERE id = %s")
+                    cur.execute(update_query, tuple(row[col] for col in columns if col != 'id') + (row['id'],))
+                conn.commit()
--- src/get_data.py
+++ src/get_data.py
@@ -13,54 +13,50 @@ db_params = {
     'host': 'localhost',
 }
 
-# Connect to your postgres DB
-conn = psycopg2.connect(**db_params)
 
-# Open a cursor to perform database operations
-cur = conn.cursor()
+# Using the with statement for managing the connection
+with psycopg2.connect(**db_params) as conn:
+    with conn.cursor() as cur:
 
-tables = create_stock_database_tables(conn, cur)
+        tables = create_stock_database_tables(conn, cur)
 
-# --- Reference Table for Ticker Symbols ----
+        # --- Reference Table for Ticker Symbols ----
 
-filename_to_index = {
-    'nasdaq_100_details.csv': 'nasdaq_100',
-    'djia_details.csv': 'djia',
-    's_and_p_500_details.csv': 's_and_p_500',
-}
-# Directory containing CSV files
-directory = 'res/indices'
+        filename_to_index = {
+            'nasdaq_100_details.csv': 'nasdaq_100',
+            'djia_details.csv': 'djia',
+            's_and_p_500_details.csv': 's_and_p_500',
+        }
+        # Directory containing CSV files
+        directory = 'res/indices'
 
-update_reference_table(conn, cur, directory, filename_to_index, reference_table=tables['reference_table'])
+        update_reference_table(conn, cur, directory, filename_to_index, reference_table=tables['reference_table'])
 
-# --- Daily Stock Quotes ----
+        # --- Daily Stock Quotes ----
 
-all_symbols = get_all_ticker_symbols(cur, reference_table=tables['reference_table'])
+        all_symbols = get_all_ticker_symbols(cur, reference_table=tables['reference_table'])
 
-# download and save ticker data
-get_ticker_data.main(
-    all_symbols,
-    save_type=save_type,
-    outputsize=outputsize,
-    conn=conn,
-    cur=cur
-)
+        # download and save ticker data
+        get_ticker_data.main(
+            all_symbols,
+            save_type=save_type,
+            outputsize=outputsize,
+            conn=conn,
+            cur=cur
+        )
 
-# --- Chart Events ----
+        # --- Chart Events ----
 
-type_to_table = {
-    'chartEvent/earnings': tables['earnings_table'],
-    'chartEvent/dividends': tables['dividends_table'],
-    'chartEvent/split': tables['split_table']
-}
+        type_to_table = {
+            'chartEvent/earnings': tables['earnings_table'],
+            'chartEvent/dividends': tables['dividends_table'],
+            'chartEvent/split': tables['split_table']
+        }
 
-scrape_events.main(
-    ticker_symbols=all_symbols,
-    tables=tables,
-    save_type=save_type,
-    conn=conn,
-    cur=cur
-)
-
-cur.close()
-conn.close()
+        scrape_events.main(
+            ticker_symbols=all_symbols,
+            tables=tables,
+            save_type=save_type,
+            conn=conn,
+            cur=cur
+        )
